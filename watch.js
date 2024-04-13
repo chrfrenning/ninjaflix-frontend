51b5933de83f93adfeeab9c8e7d8546226f23acb
Julia Mathilde Frenning <156354804+Lolrandomthings@users.noreply.github.com>
2024-04-13 09:28:12 +0000

    Implemented recommended and watch later

--- watch.js
+++ watch.js
@@ -6,32 +6,45 @@ setupVideo = function() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
 
-    getVideoInfo(id).then(function(video) {
-        currentVideo = video; // save for later
+    return new Promise((resolve, reject) => {
 
-        document.title = `${video.title} - NinjaFlix`;
-        let videoTitle = document.getElementById('video-title');
-        videoTitle.innerText = video.title;
+        getVideoInfo(id).then(function(video) {
+            currentVideo = video; // save for later
 
-        let videoContainer = document.getElementById('video-container');
-        videoElement = document.createElement('video');
-        videoElement.id = 'video';
-        videoElement.src = `${baseUrl}videos/${id}.mp4`;
-        videoElement.autoplay = false;
-        videoElement.controls = true;
-        videoContainer.append(videoElement);
+            document.title = `${video.title} - NinjaFlix`;
+            let videoTitle = document.getElementById('video-title');
+            videoTitle.innerText = video.title;
 
-        let likeCount = document.getElementById('like-count');
-        let dislikeCount = document.getElementById('dislike-count');
-        let favoriteIcon = document.getElementById('favorite-icon');
+            let videoContainer = document.getElementById('video-container');
+            videoElement = document.createElement('video');
+            videoElement.id = 'video';
+            videoElement.src = `${baseUrl}videos/${id}.mp4`;
+            videoElement.autoplay = true;
+            videoElement.controls = true;
+            videoContainer.append(videoElement);
 
-        likeCount.innerText = `${video.likes}`;
-        dislikeCount.innerText = `${video.dislikes}`;
-        if (video.favorite) {
-            favoriteIcon.classList.add('fa-solid');
-        } else {
-            favoriteIcon.classList.add('fa-regular');
-        }
+            let likeCount = document.getElementById('like-count');
+            let dislikeCount = document.getElementById('dislike-count');
+            let favoriteIcon = document.getElementById('favorite-icon');
+
+            likeCount.innerText = `${video.likes}`;
+            dislikeCount.innerText = `${video.dislikes}`;
+            if (video.favorite) {
+                favoriteIcon.classList.add('fa-solid');
+            } else {
+                favoriteIcon.classList.add('fa-regular');
+            }
+
+            let bookmarkIcon = document.getElementById('bookmark-icon');
+            if (video.bookmarked) {
+                bookmarkIcon.classList.add('fa-solid');
+            } else {
+                bookmarkIcon.classList.add('fa-regular');
+            }
+
+            resolve(video);
+        });
+
     });
 };
 
@@ -88,16 +101,60 @@ bookmarkVideo = function() {
     if (bookmarkIcon.classList.contains('fa-regular')) {
         bookmarkIcon.classList.remove('fa-regular');
         bookmarkIcon.classList.add('fa-solid');
+
+        saveBookmark(currentVideo.id, true);
     } else {
         bookmarkIcon.classList.remove('fa-solid');
         bookmarkIcon.classList.add('fa-regular');
+
+        saveBookmark(currentVideo.id, false);
     }
     
-    // TODO: Need backend support for bookmarks
+    return false;
 };
+
+addVideosToList = function(videos, listItemId = "video-list") {
+    var videoList = document.getElementById(listItemId);
+    videoList.innerHTML = '';
+
+    for (var i = 0; i < videos.length; i++) {
+        var video = videos[i];
+        var videoItem = document.createElement('li');
+        videoItem.className = 'video-list-item';
+        videoList.append(videoItem);
+
+        var videoLink = document.createElement('a');
+        videoLink.className = "video-link";
+        videoLink.href = "watch.html?id=" + video.id;
+        videoItem.append(videoLink);
+
+        var videoThumbnail = document.createElement('img');
+        videoThumbnail.src = `${baseUrl}thumbnails/${video.id}.jpg`;
+        videoThumbnail.className = "video-thumbnail";
+
+        var videoTitle = document.createElement('div');
+        videoTitle.innerText = video.title;
+        videoTitle.className = "video-title";
+
+        videoLink.append(videoThumbnail);
+        videoLink.append(videoTitle);
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     init().then(function() {
-        setupVideo();
+        setupVideo().then(function() {
+            // setup the recommended videos
+            listRecommendedVideos(currentVideo.id).then(function(videos) {
+                let recommendedVideos = document.getElementById('video-list-recommended');
+                addVideosToList(videos, 'video-list-recommended');
+            });
+            listBookmarkedVideos().then(function(videos) {
+                let bookmarkedVideos = document.getElementById('video-list-saved');
+                addVideosToList(videos, 'video-list-saved');
+            });
+        });
     });
 });
+
+
